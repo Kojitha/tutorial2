62b600ff8cc61c913470fe2a23136951d957c047
Kojitha Mandanayake <67840644+Kojitha@users.noreply.github.com>
2020-08-08 21:51:47 +0530

    tutorial 2

--- app/src/main/java/com/example/tutorial2/MainActivity.java
+++ app/src/main/java/com/example/tutorial2/MainActivity.java
@@ -3,18 +3,48 @@ package com.example.tutorial2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView textview2;
+    private static final String TAG = "MainActivity";
+
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart: ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop: ");
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        Log.i(TAG, "onPostResume: ");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "onRestart: ");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textview2= findViewById(R.id.textView2);
-
         textview2.setText(R.string.Msg2);
+        Log.i(TAG, "onCreate: ");
+
+
     }
 }
